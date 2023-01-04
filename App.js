25f8740d6fbfeba1a1ef79b49904a2b88b84efa1
Daniel Oto Diaz <danieloto212@gmail.com>
2023-01-04 02:13:37 -0300

    add API of Marvel a show the characters

--- App.js
+++ App.js
@@ -1,17 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { FlatList } from 'react-native-web';
+export default class App extends React.Component {
 
-export default function App() {
+  constructor(props){
+    super(props)
+    this.state = {
+      loading:false,
+      superHeroes: [],
+      url: 'http://gateway.marvel.com/v1/public/characters?ts=1000&apikey=640b6e70babc33bb23cc4f3208aa41db&hash=ad45a5628b415c8dd0a1a6ff6d3ad79a'
+    }
+    
+  }
+
+  componentDidMount(){
+      this.getHeroes();
+    
+  }
 
 
 
-  return (
+  getHeroes = ()=>{
+
+    this.setState({loading:true})
+
+    fetch(this.state.url)
+    .then(res => res.json())
+    .then(res => {
+
+        this.setState({
+          superHeroes : res.data.results,
+          loading: false
+        })
+
+       
+    });
+
+  }
+
+   
+
+  render() {
+    if(this.state.loading){
+      return(
+        <View style={styles.container}>
+          <Text>Download Heroes!</Text>
+          <StatusBar style="auto" />
+        </View>
+        
+        );
+    }
+    return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        data={this.state.superHeroes}
+        renderItem={
+          ({item}) =><><Text>{item.name}</Text>
+          <Image style={styles.logo} source={item.thumbnail.path + `.${item.thumbnail.extension}` }/></>
+        }
+      />
+
+      
+       
+
+    
     </View>
-  );
+    
+    );
+  }
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,6 +78,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'row',
+    justifyContent: 'start',
   },
+
+  logo:{
+    width: "100%",
+    height:"100px"
+  }
 });
